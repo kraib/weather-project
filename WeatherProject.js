4f5782b6d0dcce5fb6939142fe77227c4a8edbd5
kraiba <krabz01@gmail.com>
2016-08-09 17:14:30 +0300

    first commit

---
+++ WeatherProject.js
@@ -0,0 +1,38 @@
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput
+} from 'react-native';
+
+export default class  WeatherProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zip: "Braagg",
+    };
+  }
+  render() {
+    return (
+      <View>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
